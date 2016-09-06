2c4aed6a7688456e9364b253bb91f3012a46951d
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-09-06 16:12:04 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+console.log('scripts.js sourced');
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+
+$('.color-button').on('click', function(){
+  console.log('color-button click');
+
+$( "#outputDiv" ).append('colors');
+  })
+});
