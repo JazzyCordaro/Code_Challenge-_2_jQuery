4f1ea5b730debd42404248879b50ef11852e10ca
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-09-06 16:22:39 -0500

    red div almost

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 console.log('scripts.js sourced');
 
 
+var colors = ["red", "yellow", "green", "blue"];
+
+var redCount= 0;
+var yellowCount= 0;
+var greenCount= 0;
+var blueCount= 0;
+
+
+
 $( document ).ready(function() {
     console.log( "ready!" );
 
@@ -9,6 +18,6 @@ $( document ).ready(function() {
 $('.color-button').on('click', function(){
   console.log('color-button click');
 
-$( "#outputDiv" ).append('colors');
+$( "#outputDiv" ).append();
   })
 });
