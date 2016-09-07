989d64ea8df32127f8766ca5da62a29e094aaaeb
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-09-06 19:07:21 -0500

    another commit

--- scripts.js
+++ scripts.js
@@ -3,21 +3,33 @@ console.log('scripts.js sourced');
 
 var colors = ["red", "yellow", "green", "blue"];
 
+
+
+
+// 
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+//   })
+
+
+
+// $('.color-button').on('click', function(){
+//   console.log('color-button click');
+//
+// $( "#outputDiv" ).append();
+//   })
+// });
+
+
 var redCount= 0;
 var yellowCount= 0;
 var greenCount= 0;
 var blueCount= 0;
 
-
-
-$( document ).ready(function() {
-    console.log( "ready!" );
-
-
-
-$('.color-button').on('click', function(){
-  console.log('color-button click');
-
-$( "#outputDiv" ).append();
-  })
+$('[data-color="red"]').on('click', function(){
+    console.log(' red clicked');
+    $('<div class= "color-cube red"></div>').appendTo('.container');
+redCount++;
+console.log('redCount:', redCount);
+$('#red').html('Total red: ' + redCount);
 });
